5c492166ca8375982911267bf8ecf1f8b5671ec3
Tom Hetherington <thomas@hetheringtons.org>
2021-07-20 13:15:55 -0400

    working readme with default poller config settings

--- scripts/poller_config.py
+++ scripts/poller_config.py
@@ -15,12 +15,21 @@ class Plugin(InsitePlugin):
 
         except Exception:
 
-            from ThirtyRock_PROD_edge_def import return_reverselookup
+            # control room annotation file
+            # from ThirtyRock_PROD_edge_def import return_reverselookup
 
             params = {
-                "sdvn_natures": ["sdvn-ipg"],
-                "insite": "172.16.205.201",
-                "annotate_db": return_reverselookup(),
+                "sdvn_natures": ["sdvn-1"],
+                "insite": hosts[-1],
+                # "override": "loaded",
+                # "suppress_severity": ["info", "minor"],
+                # "suppress_known_issues": [
+                #     "QSFP 3 RX Power",
+                #     "QSFP 3 TX Power",
+                #     "QSFP 4 RX Power",
+                #     "QSFP 4 TX Power",
+                # ],
+                # "annotate_db": return_reverselookup(),
                 "magnum_cache": {
                     "nature": "mag-1",
                     "cluster_ip": "100.103.224.21",
